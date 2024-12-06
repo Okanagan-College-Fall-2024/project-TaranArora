5abba4731223c7a062622d15b69ae28dc9c1163d
Taran Arora <taransarora@gmail.com>
2024-12-06 11:54:38 -0800

    made changes to run_offline.sh

--- run_offline.sh
+++ run_offline.sh
@@ -3,5 +3,5 @@
 # This script runs the run_experiment.py with a specified model class.
 
 echo "Running the experiment with the Ollama model class..."
-python python run_experiment.py --model_class offline --model medium
+python3 python3 run_experiment.py --model_class offline --model medium
 echo "Experiment completed."
